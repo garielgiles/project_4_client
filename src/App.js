73f38d1571aa10b7e9050d7124ac85a8867ab4d2
ggiles28 <gariel.lynn@gmail.com>
2020-09-24 18:29:20 -0500

    form is working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import RMInfo from './components/RMInfo.js'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import RMInfo from "./components/RMInfo.js";
 
 function App() {
-  const [rm_infos, setInfos] = useState([])
+  const [rm_infos, setInfos] = useState([]);
+  const [formInputs, updateFormInputs] = useState({
+    name: "",
+    dimension: "",
+    content: "",
+  });
 
   const getInfos = async () => {
     try {
-      const response = await fetch('https://rick-and-mortyapi.herokuapp.com/rm_infos');
+      const response = await fetch(
+        "https://rick-and-mortyapi.herokuapp.com/rm_infos"
+      );
       const data = await response.json();
-      setInfos(data)
+      setInfos(data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  useEffect(
-    () => {
-      (async function () {
-        await getInfos();
-      }
-      )()
-    },[])
+  useEffect(() => {
+    (async function () {
+      await getInfos();
+    })();
+  }, []);
+
+  const handleChange = (event) => {
+    const updatedFormInputs = Object.assign({}, formInputs, {
+      [event.target.id]: event.target.value,
+    });
+    updateFormInputs(updatedFormInputs);
+  };
+
+  const handleSubmit = async (event) => {
+    
+    try {
+      const response = await axios.post(
+        "https://rick-and-mortyapi.herokuapp.com/rm_infos",
+        formInputs
+      );
+      const createdInfo = response.data;
+      await updateFormInputs({
+        name: "",
+        dimension: "",
+        content: "",
+      });
+      await setInfos([createdInfo, ...rm_infos]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <RMInfo rm_infos={rm_infos} />
+        <nav>
+          <h4>Post a New Character</h4>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Character</label>
+            <input
+              type="text"
+              id="name"
+              onChange={handleChange}
+              value={formInputs.name}
+            />
+            <label htmlFor="dimension">Dimension</label>
+            <input
+              type="text"
+              id="dimension"
+              onChange={handleChange}
+              value={formInputs.dimension}
+            />
+            <label htmlFor="content">Content</label>
+            <input
+              type="text"
+              id="content"
+              onChange={handleChange}
+              value={formInputs.content}
+            />
+            <input type="submit" className="submit" />
+          </form>
+        </nav>
+        <main>
+          <RMInfo rm_infos={rm_infos} />
+        </main>
       </header>
     </div>
   );
 }
 
-export default App
+export default App;
